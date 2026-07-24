4803e1d7508f33d36c286fdd7597338cda325527
keshav28-abc <keshav.tyagi.cse.2025@miet.ac.in>
2026-07-23 23:49:51 -0700

    Add files via upload

---
+++ keshav.c
@@ -0,0 +1,25 @@
+#include<stdio.h>
+
+int main(){
+    int arr[100],n,pos;
+    printf("Emter the numbers: ");
+    scanf("%d",&a[]);
+    
+    printf("Enter the elements: ");
+    for(i = 0 ;i < n ;i++){
+        scanf("%d",arr[i]);
+    }
+    for(int i=0;i<n;i++){
+        printf("%d",arr[i]);
+    }
+    printf("enter position: ");
+    scanf("%d",&pos);
+    for(int i=pos-1;i=n-1;i++){
+        arr[i]=arr[i+1];
+    }
+    n--;
+    for(int i=0;i<n;i++){
+        printf("%d",arr[i]);
+    }
+    return 0;
+}
