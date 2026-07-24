bf6f8f7d6fe4fdcb950d38f2f83c050c61ac1de8
keshav28-abc <keshav.tyagi.cse.2025@miet.ac.in>
2026-07-24 19:23:58 +0530

    Refactor array deletion program in keshav.c
    Fixed various issues including incorrect variable usage and added validation for position input.

--- keshav.c
+++ keshav.c
@@ -1,20 +1,26 @@
 #include<stdio.h>
 
 int main(){
-    int arr[100],n,pos;
+    int arr[15],n,pos;
     printf("Emter the numbers: ");
-    scanf("%d",&a[]);
+    scanf("%d",&n);
     
-    printf("Enter the elements: ");
-    for(i = 0 ;i < n ;i++){
-        scanf("%d",arr[i]);
+    printf("Enter the elements: \n");
+    for(int i = 0 ;i < n ;i++){
+        scanf("%d",&arr[i]);
     }
     for(int i=0;i<n;i++){
         printf("%d",arr[i]);
     }
-    printf("enter position: ");
+    printf("\nEnter the position of delection(1 to %d): ",n);
     scanf("%d",&pos);
-    for(int i=pos-1;i=n-1;i++){
+
+    if(pos < 1 || pos > n)
+    {
+        printf("Invalide choice");
+        return 0;
+    }
+    for(int i=pos-1;i<n-1;i++){
         arr[i]=arr[i+1];
     }
     n--;
