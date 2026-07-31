cf0e2ae432041801038225f7102fee264b68d40e
keshav28-abc <keshav.tyagi.cse.2025@miet.ac.in>
2026-07-31 11:41:26 +0530

    Fix formatting and syntax in matrix subtraction program

--- keshav-2.c
+++ keshav-2.c
@@ -2,13 +2,13 @@
 #include<stdio.h>
 int main(){
     int a[2][3],b[2][3],c[2][3],i,j;
-    ("Enter the elements of first matrix: ");
+    printf("Enter the elements of first matrix: ");
     for(i=0;i<2;i++){
         for(j=0;j<3;j++){
             scanf("%d",&a[i][j]);
         }
     }
-    ("Enter the elements of secound matrix: ");
+   printf("Enter the elements of secound matrix: ");
     for(i=0;i<2;i++){
         for(j=0;j<3;j++){
             scanf("%d",&b[i][j]);
